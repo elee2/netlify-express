a28ac673b27fa4e0b6a5d24192d105bed1338acc
elee <elee@4170trading.com>
2019-06-14 15:36:18 -0400

    timer hopefully

--- js/main.js
+++ js/main.js
@@ -2,19 +2,38 @@
 
 var supply = 0;
 var burn24 = 1;
+var distance = 100;
 
-$.ajax({
-  url:
-    "https://api-pub.bitfinex.com/v2/stats1/leo.burn.acc:1h:bal/hist?limit=24",
-  dataType: "jsonp",
+function refreshData() {
+  $.get("/.netlify/functions/server/24hr", function(data) {
+    burn24 = data.value;
+  });
+  $.get("/.netlify/functions/server/supply", function(data) {
+    supply = data.value;
+  });
 
+  distance = (supply / burn24) * 24 * 60 * 60;
+}
 
-  success: d => {
-    payload = JSON.parse(d);
-    var tburn = 0;
-    for (var i = 0; i < payload.length; ++i) {
-      tburn = tburn + payload[i][1];
-    }
-    burn24 = tburn;
-  }
+$(function() {
+  setInterval(refreshData, 10000);
 });
+
+function adjustCountDown() {
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  $(".item").html(
+    days +
+      "days " +
+      hours +
+      "hours " +
+      minutes +
+      "minutes " +
+      seconds +
+      "seconds "
+  );
+  distance = distance - 1;
+}
